44b2a1352f2ed6a7e682e1b9283957153e36049a
Sofia Fateyev <pateyev.sofia@gmail.com>
2019-10-26 20:04:44 +0200

    Add interactivity
    Receive an error message when:
    -The input field is empty
    -The email address is not formatted correctly

---
+++ js/app.js
@@ -0,0 +1,66 @@
+let btn = document.querySelector('.sign-btn');
+let emailInput = document.querySelector('.input');
+let form = document.querySelector('form');
+let emailContainer = document.querySelector('.form-container');
+
+function isValid(email) {
+  let regex = /[^@\s]+@[^@\s]+/;
+  let result = email.match(regex);
+  if (result) {
+    return true;
+  }
+  return false;
+}
+
+function addValidationMsg(msg, cssClass) {
+  let inputForm = document.querySelector('.sign-up');
+  let oldValidation = inputForm.querySelector('.validation');
+  let oldIconError = inputForm.querySelector('.icon-error');
+
+  if (oldValidation) {
+    oldValidation.remove();
+    emailContainer.classList.remove('not-valid');
+  }
+
+  if (oldIconError) {
+    oldIconError.remove();
+  }
+
+  let paragraph = document.createElement('p');
+  paragraph.innerHTML = msg;
+  paragraph.classList.add('validation', cssClass);
+  form.appendChild(paragraph);
+}
+
+function remStyle() {
+  let olsSelectedStyle = document.querySelector('.selected');
+
+  if (olsSelectedStyle) {
+    emailContainer.classList.remove('selected');
+  }
+}
+
+emailInput.addEventListener('click', () => {
+  emailContainer.classList.add('selected');
+});
+
+emailInput.addEventListener('focusout', () => {
+  emailContainer.classList.remove('selected');
+});
+
+btn.addEventListener('click', () => {
+  remStyle();
+
+  if (isValid(emailInput.value)) {
+    return addValidationMsg('Successfully sent!', 'message');
+  }
+  addValidationMsg('Please provide a valid email', 'error-message');
+
+  emailContainer.classList.add('not-valid');
+
+  let iconError = document.createElement('img');
+  iconError.setAttribute('src', 'images/icon-error.svg');
+  iconError.setAttribute('alt', 'Error icon');
+  iconError.classList.add('icon-error');
+  form.appendChild(iconError);
+});
